056c1fb5e9e43279584baf6d68e0840795e6c5a3
Robert Pliszka <robplisz@gmail.com>
2026-09-03 20:53:44 -0400

    Created if statement to do certain task depending on user choice

--- main.py
+++ main.py
@@ -33,15 +33,27 @@ resources = {
 }
 
 machine_money = 0.00
+machine_running = True
 
+while machine_running:
 # TODO: Prompt user to choose what coffee they want
-user_coffee = input("What would you want? (esspresso/latte/cappuccino): ").lower()
-
-# TODO: Turn off machine by typing off
+    user_coffee = input("What would you want? (espresso/latte/cappuccino): ").lower()
 
 # TODO: Show machine supplies when typing in report
-if user_coffee == "report":
-    print(f"Water: {resources['water']}ml \n Milk: {resources['milk']}ml \n Coffee: {resources['coffee']}g \n Money: ${machine_money}")
+    if user_coffee == "report":
+        print(f"Water: {resources['water']}ml \n Milk: {resources['milk']}ml \n Coffee: {resources['coffee']}g \n Money: ${machine_money}")
+# TODO: Turn off machine by typing off
+    elif user_coffee == "off":
+        machine_running = False
+    elif user_coffee == "espresso":
+        print("Espresso")
+    elif user_coffee == "latte":
+        print("Latte")
+    elif user_coffee == "cappuccino":
+        print("Cappuccino")
+    else:
+        print("Please enter a valid option")
+
 
 
 # TODO: Process coins inserted by user
