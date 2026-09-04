45f749a43e1f6ca84f0c0a750452029bdac3c8db
Robert Pliszka <robplisz@gmail.com>
2026-09-03 20:44:07 -0400

    Created prompt to ask user for the coffee options. Created a report print for user to run

--- main.py
+++ main.py
@@ -32,3 +32,20 @@ resources = {
     "coffee": 100,
 }
 
+machine_money = 0.00
+
+# TODO: Prompt user to choose what coffee they want
+user_coffee = input("What would you want? (esspresso/latte/cappuccino): ").lower()
+
+# TODO: Turn off machine by typing off
+
+# TODO: Show machine supplies when typing in report
+if user_coffee == "report":
+    print(f"Water: {resources['water']}ml \n Milk: {resources['milk']}ml \n Coffee: {resources['coffee']}g \n Money: ${machine_money}")
+
+
+# TODO: Process coins inserted by user
+
+# TODO: Check if tansaction successful and give user output
+
+# TODO: Make Coffee
